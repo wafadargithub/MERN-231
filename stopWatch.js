e56c1f8a509167c1f02e9d956bbd0d4ee60fdcfc
wafadar hussain <124126541+wafadargithub@users.noreply.github.com>
2024-10-22 17:25:01 +0500

    Add files via upload

---
+++ stopWatch.js
@@ -0,0 +1,135 @@
+// let hours = 0;
+// let minutes = 0;
+// let seconds = 0;
+// let intervalId;
+// let running = false;
+
+// function updateDisplay() {
+//     const hrs = hours < 10 ? `0${hours}` : hours;
+//     const mins = minutes < 10 ? `0${minutes}` : minutes;
+//     const secs = seconds < 10 ? `0${seconds}` : seconds;
+//     console.clear(); // Clears the console to show the latest time
+//     console.log(`${hrs}:${mins}:${secs}`);
+// }
+
+// function startStopwatch() {
+//     if (!running) {
+//         running = false;
+//         intervalId = setInterval(() => {
+//             seconds++;
+//             if (seconds === 60) {
+//                 seconds = 0;
+//                 minutes++;
+//                 if (minutes === 60) {
+//                     minutes = 0;
+//                     hours++;
+//                 }
+//             }
+//             updateDisplay();
+//         }, 1000);
+//     }
+// }
+
+// function stopStopwatch1() {
+//     clearInterval(intervalId);
+//     running = false;
+// }
+
+// function resetStopwatch() {
+//     clearInterval(intervalId);
+//     running = false;
+//     hours = 0;
+//     minutes = 0;
+//     seconds = 0;
+//     updateDisplay();
+// }
+
+// // Example of how to use in JavaScript console:
+// startStopwatch(); // to start
+// stopStopwatch1(); // to stop
+// resetStopwatch(); // to reset
+
+
+
+// // var hours=0;
+// // var minutes=0;
+// // var seconds=0;
+// // var intervalId;
+// // var running = false;
+
+// // function updateDisplay(){
+// //    const hrs=hours<10 ? 0${hours} : hours;
+// //    const mins=minutes<10 ? 0${minutes} : minutes;
+// //    const secs=seconds<10 ? 0${seconds} :seconds;
+// //    console.clear();
+// //    console.log(${hrs}:${mins}:${secs}) ; 
+// // }
+
+// // function startStopwatch(){
+// //     if(!running){
+// //         running=true;
+// //       intervalId=setInterval(()=>{
+// //       seconds++;
+// //       if(seconds===60){
+// //         seconds=0;
+// //         minutes++;
+// //         if(minutes===60){
+// //             minutes=0;
+// //             hours++;
+// //         }
+// //       }
+// //       updateDisplay();      
+// //       },1000)
+// //     }
+// // }
+// //       function stopStopwatch(){
+// //         clearInterval(intervalId);
+// //         running=false;
+// //       }
+// //       function resetStopwatch(){
+// //             clearInterval(intervalId)
+// //                 running=false;
+// //                 hours=0;
+// //                 minutes=0;
+// //                 seconds=0;
+// //                 updateDisplay();
+            
+// //       }
+
+
+// //  startStopwatch();
+// //  stopStopwatch();
+// //  resetStopwatch();
+
+
+
+
+
+const startStopwatch = () => {
+    let starttime = new Date().getTime();
+    //console.log(starttime);
+    
+  
+    const updateStopwatch = () => {
+      new Promise((res) => {
+        setTimeout(() => {
+          let time = new Date().getTime();
+         // console.log(time);
+          
+          let timeshow = Math.floor((time - starttime) / 1000); 
+          console.log(timeshow);
+          res(); 
+        }, 1000)
+      }).then(() => {
+        updateStopwatch();
+      })
+    }
+  
+    updateStopwatch(); 
+  }
+  
+  const main = () => {
+    startStopwatch();
+  }
+  
+  main();
